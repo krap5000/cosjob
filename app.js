395bdadaa49b8152c3bb682a2f996e46c3dc87a7
krap5000 <krap5000@gmail.com>
2021-03-10 01:21:00 -0500

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const cors = require("cors");
 
 const myCOS = require("ibm-cos-sdk");
 
-var i = process.env.JOB_INDEX;
+var i = 1; process.env.JOB_INDEX;
 console.log("my index is: " + i);
 
 getCosClient();
