33b57c6ca864adb3ad1cf7d68503773db1ad5af5
krap5000 <krap5000@gmail.com>
2021-03-09 23:40:51 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,9 @@
 "use strict";
-//const express = require("express");
-//const app = express();
-//const request = require("request");
-//const path = require("path");
+
 require("dotenv").config({
   silent: true
 });
 const cors = require("cors");
-//app.use(cors());
-//app.use(express.static(__dirname + '/public/js'));
-//app.use(express.static(__dirname + '/public/images'))
-//app.use(express.static(__dirname + '/public/css'))
-//const port = process.env.PORT || 3000;
 
 
 const myCOS = require("ibm-cos-sdk");
@@ -65,35 +57,4 @@ function getItem(bucketName, itemName) {
 
 
 
-/*
- * Default route for the web app
- *
-app.get('/', function(req, res) {
 
-        res.sendFile(__dirname + "/public/index.html");
-
-});
-
-
-
-app.get("/results", async (req, res, next) => {
-  try {
-    //let result = await getBucketContents(req, res, next, "results");
-    //res.send(result);
-    let result = await getItem("test1-bucket-code-engine", "test1.txt")
-    res.send(result);
-    console.log(result)
-  } catch (error) {
-    // Passes errors into the error handler
-    console.log(error);
-    return next(error);
-  }
-});
-
-
-//app.use(function(error, req, res, next) {
-  //res.status(500).send(error.message);
-//});
-
-//app.listen(port, () => console.log(`App listening on port ${port}!`));
-*/
